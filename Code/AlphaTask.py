7412f5d4e2ac36057da9e62f5797616ef7b19f62
JasonS76 <jsedluk01@gmail.com>
2019-01-22 22:23:37 -0500

    Update AlphaTask.py

--- Code/AlphaTask.py
+++ Code/AlphaTask.py
@@ -1,1 +1,19 @@
+From Motors import *
+From Servo import *
 
+os.system ("sudo pigpiod") #Launching GPIO library needed for motors
+time.sleep(1) #needed delay to setup
+
+
+forward()
+time.sleep(2)
+turn_left()
+time.sleep(2)
+stop_turning()
+still()
+time.sleep(2)
+turn_right()
+time.sleep(2)
+stop_turning()
+still()
+stop()
