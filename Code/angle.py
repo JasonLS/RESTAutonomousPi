59bf3646ef9e9a4638c3cf18775981db00dd55db
Jason <jsedluk01@gmail.com>
2019-02-24 08:20:08 -0500

    add angle file for servo testing

---
+++ Code/angle.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+from time import sleep
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+servoPIN = 14
+GPIO.setup(servoPIN, GPIO.OUT)
+pwm=GPIO.PWM(servoPIN, 50)
+pwm.start(0)
+
+
+
+def SetAngle(angle):
+    duty = angle / 18 + 2
+    GPIO.output(servoPIN, True)
+    pwm.ChangeDutyCycle(duty)
+    sleep(1)
+    GPIO.output(servoPIN, False)
+    pwm.ChangeDutyCycle(0)#Remove this?
+
+while 1:
+    val = int(input("Input value: \n"))
+    if(val > 0):
+        SetAngle(val)
+    else:
+        break
