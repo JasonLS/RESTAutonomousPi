d3f33136cc29e1f1f9a58d7bc39933a62d7f9585
Jason <jsedluk01@gmail.com>
2019-03-27 23:10:41 -0400

    Updated turn functions to prevent overturning

--- Code/Servo.py
+++ Code/Servo.py
@@ -8,7 +8,7 @@ Servo_Pin = 14
 
 GPIO.setup(Servo_Pin, GPIO.OUT)
 pwm=GPIO.PWM(Servo_Pin, 50) #Sets pin 14 at 50Hz. Configures GPIO to PWM
-pwm.start(150) #150 = nothing 
+pwm.start(10) #150 = nothing 
 
 
 
@@ -20,27 +20,40 @@ def SetAngle(angle): #Formula for exact angles so it always turns the same degre
     GPIO.output(Servo_Pin, False)
     pwm.ChangeDutyCycle(0) #Remove this? NO, this makes it so the servo stops turning. Otherwise, it will continue to turn.
 
-Turn_Value = 2 #2 for neutral/straight 1 for left, 3 for right
+Turn_Value = 2
+
 
 def turn_left():
+    global Turn_Value
     if Turn_Value != 1:
         print("Left")
         SetAngle(100)
         Turn_Value =  1
 
 def turn_right():
+    global Turn_Value
+    
     if Turn_Value != 3:
         print("Right")
         SetAngle(200)
         Turn_Value = 3
 
-def stop_turning():
+def center():
+    global Turn_Value
+
     if Turn_Value != 2:
-        print("Stop Turning")
-        SetAngle(150)
-        Turn_Value = 2
+        print("Centering")
+        if Turn_Value == 1: #Left
+            turn_right()
+            Turn_Value = 2
+        if Turn_Value == 3: #Right
+            turn_left()
+            Turn_Value = 2
+        
 
 def cleanup():
+    global Turn_Value
+
     print("Exiting Servo...")
     if Turn_Value == 1:
         SetAngle(200)
